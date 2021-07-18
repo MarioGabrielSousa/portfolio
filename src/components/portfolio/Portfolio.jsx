55a516ecafc2af828627e7b7e983120cb1306efb
mario sousa <vegetalizado@gmail.com>
2021-07-18 21:00:21 +0000

    Intro done, portfolio styling

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,71 @@
+import PortfolioList from "../portfolioList/PortfolioList";
 import "./portfolio.scss";
+import { useEffect, useState } from "react";
 
 export default function Portfolio() {
-  return <div className="portfolio" id="portfolio"></div>;
+  const [selected, setSelected] = useState("featured");
+  const list = [
+    {
+      id: "featured",
+      title: "Featured",
+    },
+    {
+      id: "web",
+      title: "Web APP",
+    },
+    {
+      id: "mobile",
+      title: "Mobile App",
+    },
+    {
+      id: "design",
+      title: "Design",
+    },
+    {
+      id: "content",
+      title: "Content",
+    },
+  ];
+
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        {list.map((item) => (
+          <PortfolioList
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id}
+          />
+        ))}
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="assets/gameBg.jpg" alt="" />
+          <h3>Item title</h3>
+        </div>
+        <div className="item">
+          <img src="assets/gameBg.jpg" alt="" />
+          <h3>Item title</h3>
+        </div>
+        <div className="item">
+          <img src="assets/gameBg.jpg" alt="" />
+          <h3>Item title</h3>
+        </div>
+        <div className="item">
+          <img src="assets/gameBg.jpg" alt="" />
+          <h3>Item title</h3>
+        </div>
+        <div className="item">
+          <img src="assets/gameBg.jpg" alt="" />
+          <h3>Item title</h3>
+        </div>
+        <div className="item">
+          <img src="assets/gameBg.jpg" alt="" />
+          <h3>Item title</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -7,7 +7,7 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
-            .log
+            d•t
           </a>
           <div className="itemContainer">
             <PhoneAndroid className="icon" />
